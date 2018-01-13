d4c567d154902b7e1a4f6f30f575d829a221c2df
Peter Neely <pgneely@gmail.com>
2018-01-13 10:18:19 -0800

    Working build

---
+++ src/app/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+// This is a class-based component because the current
+// version of hot reloading won't hot reload a stateless
+// component at the top-level.
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {'App'}
+      </div>
+    );
+  }
+}
+
+// App.propTypes = {
+
+// };
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/default */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import configureStore, { history } from './store/configureStore';
+import initialState from './store/initialState';
+import Root from './Root';
+require('./favicon.ico');
+
+const store = configureStore(initialState);
+
+render(
+  <AppContainer>
+    <Root store={store} history={history} />
+  </AppContainer>,
+  document.getElementById('app')
+);
+
+if (module.hot) {
+  module.hot.accept('./Root', () => {
+    const NewRoot = require('./Root').default;
+    render(
+      <AppContainer>
+        <NewRoot store={store} history={history} />
+      </AppContainer>,
+      document.getElementById('app')
+    );
+  });
+}
